23078d356c5cc70272206bc82bb2f3f02aa06f26
Jl_Khai <jlzamliankhai@email.com>
2023-06-13 12:30:38 +0630

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Google Pixel Timer UI
+
+## Tools
+
+- Tailwind Css
+- React-Icons
+
+## What I Build
+
+A google pixel phone alike timer UI.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Timer from './Timer'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Timer />
+    </>
+  )
+}
+
+export default App
